460b0c5865eb799a91a5a4feb5e9722e2b32d46e
Mika Rinne <mika.rinne@oracle.com>
2026-04-09 10:22:34 +0300

    tf version upgrade

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "6.18.0"
+      version = "~> 8.8.0"
       configuration_aliases = [oci.home]
     }
   }
